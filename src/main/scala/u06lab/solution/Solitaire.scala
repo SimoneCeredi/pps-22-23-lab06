c52d6e458945241b7a645a27f280b2f33d6738ae
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-04-20 15:15:28 +0200

    Small solitaire improvements

--- src/main/scala/u06lab/solution/Solitaire.scala
+++ src/main/scala/u06lab/solution/Solitaire.scala
@@ -1,27 +1,24 @@
 package u06lab.solution
 
-import u06lab.solution.Solitaire.{h, w}
-
 object Solitaire extends App:
   type Mark = (Int, Int)
   type Solution = List[Mark]
   type IterableFactory = Solution => Seq[Solution]
-  val w = 5
+  val w = 7
   val h = 5
 
   val moves = List((2, 2), (2, -2), (-2, -2), (-2, 2), (0, 3), (3, 0), (-3, 0), (0, -3))
 
-  given IterableFactory = Seq(_)
+  given IterableFactory = LazyList(_)
 
-  val sol = placeMarks()
-  sol.zipWithIndex foreach printSolution
+  placeMarks().zipWithIndex foreach printSolution
 
-  def placeMarks(w: Int = w, h: Int = h, solLength: Int = w * h)(using factory: IterableFactory): Seq[Solution] =
+  def placeMarks(solLength: Int = w * h)(using factory: IterableFactory): Seq[Solution] =
     solLength match
       case 1 => factory(List((w / 2, h / 2)))
       case _ =>
         for
-          solution <- placeMarks(solLength = solLength - 1)
+          solution <- placeMarks(solLength - 1)
           move <- moves
           lastMark = solution.last
           mark = (lastMark._1 + move._1, lastMark._2 + move._2)
@@ -30,10 +27,11 @@ object Solitaire extends App:
           solution :+ mark
 
   def isLegitimate(mark: Mark, others: Solution): Boolean =
-    mark._1 >= 0 && mark._1 < w && mark._2 >= 0 && mark._2 < h
-      && !(others contains mark)
+    isInsideBoard(mark) && !(others contains mark)
+
+  def isInsideBoard(mark: Mark): Boolean = mark._1 >= 0 && mark._1 < w && mark._2 >= 0 && mark._2 < h
 
   def printSolution(si: (Solution, Int)): Unit =
-    println(s"sol ${si._2}")
+    println(s"sol ${si._2} -> length: ${si._1.length}")
     println(si._1)
 
