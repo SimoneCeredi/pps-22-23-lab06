7f24dd3277088c85ccc82d21b950b769cef55a29
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-04-21 10:29:02 +0200

    Changed render for solitaire

--- src/main/scala/u06lab/solution/Solitaire.scala
+++ src/main/scala/u06lab/solution/Solitaire.scala
@@ -4,14 +4,14 @@ object Solitaire extends App:
   type Mark = (Int, Int)
   type Solution = List[Mark]
   type IterableFactory = Solution => Seq[Solution]
-  val w = 7
+  val w = 5
   val h = 5
 
   val moves = List((2, 2), (2, -2), (-2, -2), (-2, 2), (0, 3), (3, 0), (-3, 0), (0, -3))
 
-  given IterableFactory = LazyList(_)
+  given IterableFactory = Seq(_)
 
-  placeMarks().zipWithIndex foreach printSolution
+  placeMarks().zipWithIndex foreach (s => println(s"\nSol #${s._2}\n${render(s._1, w, h)}"))
 
   def placeMarks(solLength: Int = w * h)(using factory: IterableFactory): Seq[Solution] =
     solLength match
@@ -31,7 +31,13 @@ object Solitaire extends App:
 
   def isInsideBoard(mark: Mark): Boolean = mark._1 >= 0 && mark._1 < w && mark._2 >= 0 && mark._2 < h
 
-  def printSolution(si: (Solution, Int)): Unit =
-    println(s"sol ${si._2} -> length: ${si._1.length}")
-    println(si._1)
+  def render(solution: Solution, width: Int, height: Int): String =
+    val rows =
+      for y <- 0 until height
+          row = for x <- 0 until width
+                    number = solution.indexOf((x, y)) + 1
+          yield if number > 0 then "%-2d ".format(number) else "X  "
+      yield row.mkString
+    rows.mkString("\n")
+
 
